29f934727942706d06f8eafc13136d2f65a45fd4
kmurerwa <xwaxes@gmail.com>
2022-07-09 15:21:24 +0300

    feat: Add image assets and global config to the app

--- Formula All/Core/AppDelegate.swift
+++ Formula All/Core/AppDelegate.swift
@@ -10,12 +10,22 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setUpGlobalViews()
+        
         return true
     }
+    
+    private func setUpGlobalViews() {
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+    }
+    
 
     // MARK: UISceneSession Lifecycle
 
