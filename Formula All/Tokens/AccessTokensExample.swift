8c47029dec3b9980bab294fcb1c8ff3bf1011752
kmurerwa <xwaxes@gmail.com>
2022-08-10 12:50:26 +0300

    feat: Add ability to create new access tokens

---
+++ Formula All/Tokens/AccessTokensExample.swift
@@ -0,0 +1,12 @@
+//
+//  AccessTokensExample.swift
+//  Formula All
+//
+//  Created by Kenneth Murerwa on 10/08/2022.
+//
+
+import Foundation
+
+class AccessTokensExample { // Rename this class to 'AccessTokens' for the app to work
+    static let ACCESS_TOKEN = "" // Add your access token here
+}
